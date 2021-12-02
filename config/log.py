fa6df545958b5453766846972556afe2e19eef22
liubo <liubo@yuewen.com>
2021-12-02 15:13:02 +0800

    日志->调整配置逻辑

--- config/log.py
+++ config/log.py
@@ -3,25 +3,18 @@ import logging
 import logging.config
 import os
 import yaml
-from pprint import pprint
-
 
 def setup_logger(log_root_path, config_file):
     '''
         log_root_path:日志目录名称
         config_file:配置文件yaml路径
     '''
-    if not os.path.exists(os.path.dirname(log_root_path)):
-        os.makedirs(os.path.dirname(log_root_path))
+
+    if not os.path.exists(log_root_path):
+        os.makedirs(log_root_path)
 
     with open(config_file,'r') as fh:
         logging_config = yaml.safe_load(fh)
-
-    #设置handler的filepath
-    for handler in logging_config['handlers']:
-        if logging_config['handlers'][handler].get("filename"):
-            log_name = logging_config['handlers'][handler]['filename']
-            logging_config['handlers'][handler]['filename'] = os.path.join(log_root_path,log_name)
 
     logging.config.dictConfig(logging_config)
 
@@ -29,13 +22,16 @@ def setup_logger(log_root_path, config_file):
 
 def init(*loggers):
     APP_DIR = os.path.dirname(os.path.abspath(__file__))
-    log_root_path = os.path.join(APP_DIR, 'logs')
+    BASE_DIR = os.path.dirname(APP_DIR)
+    log_root_path = os.path.join(BASE_DIR, 'logs')
     config_file = os.path.join(APP_DIR,'logging.yaml')
     setup_logger(log_root_path,config_file)
 
     return [logging.getLogger(logger_name) for logger_name in loggers]
 
 server_logger, cron_logger = init("server","cron")
+
+
 if __name__ == "__main__":
 
     cron_del_logger = logging.getLogger("cron.delete")
